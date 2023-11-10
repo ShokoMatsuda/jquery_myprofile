a02937cc64dfe36cf1d665b24884bb74910a2ffe
ShokoMatsuda <syokom.0128@gmail.com>
2023-11-10 16:23:15 +0900

    ナビゲーションのhover実装

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-// カルーセル
-$('.carousel').slick({
-    autoplay: true,
-    dots: true,
-    infinite: true,
-    autoplaySpeed: 5000,
-    arrows: false,
-    fade: true,
+$(function() {
+    // カルーセル
+    $('.carousel').slick({
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        fade: true,
+    });
+
+    // ホバー
+    $('.link_hover').hover(
+        function() {
+            $(this).fadeTo('slow', 0.5);
+        },
+        function() {
+            $(this).fadeTo('slow', 1.0);
+        }
+    );
 });
